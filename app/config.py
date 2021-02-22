bfe227311a216dca5ebed422c80a09887697d719
Jello98 <telljelani@gmail.com>
2021-02-21 20:13:17 -0500

    lab 3 done

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = os.environ.get('MAIL_PORT') or '25'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
